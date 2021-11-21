3e2066fc4684f68da02f47b5a9a154c87a9c2788
HealthHighway <68342984+HealthHighway@users.noreply.github.com>
2021-11-21 14:00:33 +0530

    first commit

---
+++ android/build/generated/source/buildConfig/release/com/calendarevents/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.calendarevents;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.calendarevents";
+  public static final String BUILD_TYPE = "release";
+}
